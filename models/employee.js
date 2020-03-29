13e732e091ccd5e0e7fa03214d2fefa85b915133
anz213 <56008225+anzook@users.noreply.github.com>
2020-03-29 12:05:29 -0400

    routes tested

--- models/employee.js
+++ models/employee.js
@@ -1,71 +1,83 @@
-
 module.exports = function(sequelize, DataTypes) {
-    const Employee = sequelize.define("employee", {
-        firstName: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            // ====Validations====
-            validate: {
-              len: [1, 40],
-            },
-          },
-          lastName: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            // ====Validations====
-            validate: {
-              len: [1, 40],
-            },
-          }, 
-          email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-              isEmail: true,
-            }
-          }, 
-          phone: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-              is: ["^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$", 'i']
-            }
-          }, 
-        status: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          isIn: [[
-            'permanent', 'temporary', 'intern', 'contract'
-          ]]
+  const Employee = sequelize.define("employee", {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      // ====Validations====
+      validate: {
+        len: [1, 40]
+      }
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      // ====Validations====
+      validate: {
+        len: [1, 40]
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        validatePhone: function(value) {
+          // regex validation for US Phone number that accept a dot, a space, a dash, a forward slash deliminators
+          // can put 0 or 1 in front
+          if (
+            !/^((\(\d{3}\)?)|(\d{3}))([\s-./]?)(\d{3})([\s-./]?)(\d{4})?$/i.test(
+              value
+            )
+          ) {
+            throw new Error("phone format error!");
+          }
         }
-      },
-      office: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        // ====Validations====
-        validate: {
-          len: [1, 50],
-        },
-      },
-      department: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          isIn: [[
-            'management', 'engineering', 'marketing', 'sales', 'customer service', 'internal services'
-          ]]
-        }
-      },
-    });
+      }
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isIn: [["permanent", "temporary", "intern", "contract"]]
+      }
+    },
+    office: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      // ====Validations====
+      validate: {
+        len: [1, 50]
+      }
+    },
+    department: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isIn: [
+          [
+            "management",
+            "engineering",
+            "marketing",
+            "sales",
+            "customer service",
+            "internal services"
+          ]
+        ]
+      }
+    }
+  });
 
-
-    return Employee;
-  };
-    
+  return Employee;
+};
